755ee3c4d9f0aec3ce7f017419513d1595c0dd86
Jacob Pretorius <jacob@jcpretorius.com>
2021-04-10 11:39:53 +0100

    init

---
+++ src/Custom/display_number.65C02.asm
@@ -0,0 +1,162 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+value = $0200 // 2 bytes
+mod10 = $0202 // 2 bytes
+message = $0204 // 6 bytes
+
+E  = %10000000
+RW = %01000000
+RS = %00100000
+
+  .org $8000
+
+
+reset:
+  ldx #$ff
+  txs
+
+  lda #%11111111 // Set all pins on port B to output
+  sta DDRB
+  lda #%11100000 // Set top 3 pins on port A to output
+  sta DDRA
+
+  lda #%00111000 // Set 8-bit mode// 2-line display// 5x8 font
+  jsr lcd_instruction
+  lda #%00001110 // Display on// cursor on// blink off
+  jsr lcd_instruction
+  lda #%00000110 // Increment and shift cursor// don't shift display
+  jsr lcd_instruction
+  lda #$00000001 // Clear display
+  jsr lcd_instruction
+
+  lda #0
+  sta message
+  // Init value to be the number to convert
+  lda number
+  sta value
+  lda number + 1
+  sta value + 1
+
+divide:
+  lda #0
+  sta mod10
+  sta mod10 + 1
+  clc
+
+  ldx #16
+divloop:
+  // Rotate quotient and remainder
+  rol value
+  rol value + 1
+  rol mod10
+  rol mod10 + 1
+
+  // a,y = divident - divisor
+  sec
+  lda mod10
+  sbc #10
+  tay // Save low byte in Y
+  lda mod10 + 1
+  sbc #0
+  bcc ignore_result // Branch if divident < divisor
+  sty mod10
+  sta mod10 + 1
+
+ignore_result
+  dex
+  bne divloop
+  rol value // Shift in the last bit of the quotient
+  rol value + 1
+
+  lda mod10
+  clc
+  adc #"0"
+  jsr push_char
+
+  // If value != 0 then continue
+  lda value
+  ora value + 1
+  bne divide // Branch if value not zero
+
+  ldx #0
+print:
+  lda message,x
+  beq prgdone
+  jsr print_char
+  inx
+  jmp print
+
+prgdone:
+  jmp prgdone
+
+number :.word 2078
+
+// Add the char in A reg to the begining of null-term
+// string 'message'
+push_char:
+  pha // Push new first char to stack
+  ldy #0
+
+char_loop:
+  lda message,y // Get char on string and put into X
+  tax
+  pla
+  sta message,y // Pull char off stack and add to string
+  iny
+  txa
+  pha // Push char from string onto stack
+  bne char_loop
+
+  pla
+  sta message,y // Pull null off stack and add to end of string
+  rts
+
+lcd_wait:
+  pha
+  lda #%00000000  // Port B is input
+  sta DDRB
+
+lcdbusy:
+  lda #RW
+  sta PORTA
+  lda #(RW | E)
+  sta PORTA
+  lda PORTB
+  and #%10000000
+  bne lcdbusy
+
+  lda #RW
+  sta PORTA
+  lda #%11111111  // Port B is output
+  sta DDRB
+  pla
+  rts
+
+lcd_instruction:
+  jsr lcd_wait
+  sta PORTB
+  lda #0         // Clear RS/RW/E bits
+  sta PORTA
+  lda #E         // Set E bit to send instruction
+  sta PORTA
+  lda #0         // Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+print_char:
+  jsr lcd_wait
+  sta PORTB
+  lda #RS         // Set RS// Clear RW/E bits
+  sta PORTA
+  lda #(RS | E)   // Set E bit to send instruction
+  sta PORTA
+  lda #RS         // Clear E bits
+  sta PORTA
+  rts
+
+  .org $fffc
+  .word reset
+  .word $0000
